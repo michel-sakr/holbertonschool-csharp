9b0c687bb1a3b34a3c3afe95158bd208572de2bd
Michel Sakr <michel.s.sakr@gmail.com>
2021-07-13 19:43:04 +0300

    initial commit

---
+++ 0x01-csharp-ifelse_loops_methods/1-last_digit/1-last_digit.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace _1_last_digit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rndm = new Random();
+            int number = rndm.Next(-10000, 10000);
+            int last_digit = number % 10;
+            Console.Write("The last digit of " + number + " is ");
+            if (last_digit > 5)
+            {
+                Console.WriteLine(last_digit + " and is greater than 5");
+            }
+            else if (last_digit == 0)
+            {
+                Console.WriteLine(last_digit + " and is 0");
+            }
+            else if (last_digit < 6 && last_digit != 0)
+            {
+                Console.WriteLine(last_digit + " and is less than 6 and not 0");
+            }
+        }
+    }
+}
---
+++ 0x01-csharp-ifelse_loops_methods/2-print_alphabet/2-print_alphabet.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace _2_print_alphabet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char letter = 'a';
+            for (int i = 0; i < 26; i++)
+            {
+                Console.Write(letter++);
+            }
+        }
+    }
+}
