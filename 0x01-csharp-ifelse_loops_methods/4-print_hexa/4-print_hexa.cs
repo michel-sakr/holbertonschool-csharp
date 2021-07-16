5160fcb265740badef340b840e8cdb051859147e
Michel Sakr <michel.s.sakr@gmail.com>
2021-07-16 11:08:11 +0300

    update

--- 0x01-csharp-ifelse_loops_methods/4-print_hexa/4-print_hexa.cs
+++ 0x01-csharp-ifelse_loops_methods/4-print_hexa/4-print_hexa.cs
@@ -6,7 +6,7 @@ namespace _4_print_hexa
     {
         static void Main(string[] args)
         {
-            for (int i = 0; i < 98; i++)
+            for (int i = 0; i < 99; i++)
             {
                 string hexval = i.ToString("x");
                 Console.WriteLine(i + " = " + "0x" + hexval);
