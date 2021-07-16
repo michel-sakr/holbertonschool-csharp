31f63559ab6891b1eaa0d7ccce621d8b5349c5da
Michel Sakr <michel.s.sakr@gmail.com>
2021-07-16 10:50:07 +0300

    update

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/0-unique_add/0-unique_add.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    // Main - entry point
+    static void Main(string[] args)
+    {
+        List<int> myList1 = new List<int>() { 1, 1, 2, 3, 4, 5, 6, 6, 7, 7, 8 };
+        List<int> myList2 = new List<int>() { 98, 98, -14, 972, 0, -42, -972 };
+
+        Console.WriteLine(List.Sum(myList1));
+        Console.WriteLine(List.Sum(myList2));
+    }
+}
+
+class List
+{
+
+    public static int Sum(List<int> myList)
+    {
+
+        int result = 0;
+        while (myList.Count > 0)
+        {
+            result += myList[0];
+            myList.RemoveAll(j => j == myList[0]);
+        }
+        return result;
+    }
+}
---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/3-stack_push_pop/3-stack_push_pop.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    // Main - entry point
+    static void Main(string[] args)
+    {
+        Stack<string> aStack = new Stack<string>();
+
+        aStack.Push("C");
+        aStack.Push("HTML");
+        aStack.Push("Javascript");
+        aStack.Push("Python");
+        aStack.Push("React");
+        aStack.Push("Ruby");
+
+        foreach (string item in aStack)
+            Console.WriteLine(item);
+
+        Console.WriteLine("------");
+
+        MyStack.Info(aStack, "C#", "Javascript");
+
+        Console.WriteLine("------");
+
+        foreach (string item in aStack)
+            Console.WriteLine(item);
+    }
+}
+
+class MyStack
+{
+    public static Stack<string> Info(Stack<string> aStack, string newItem, string search)
+    {
+        Console.WriteLine("Number of items: " + aStack.Count);
+
+        if (aStack.Count == 0)
+            Console.WriteLine("Stack is empty");
+        else
+            Console.WriteLine("Top item: " + aStack.Peek());
+
+        Console.WriteLine("Stack contains " + search + ": " + aStack.Contains(search));
+
+        if (aStack.Contains(search))
+        {
+            string s = "";
+            while (!s.Equals(search))
+            {
+                s = aStack.Pop();
+            }
+        }
+
+        aStack.Push(newItem);
+        return aStack;
+
+    }
+}
