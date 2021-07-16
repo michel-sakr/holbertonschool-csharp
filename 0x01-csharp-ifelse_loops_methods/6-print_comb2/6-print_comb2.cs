c13e5713fc09a0f735d35743c2153b9161d86e7a
Michel Sakr <michel.s.sakr@gmail.com>
2021-07-16 11:14:48 +0300

    update

--- 0x01-csharp-ifelse_loops_methods/6-print_comb2/6-print_comb2.cs
+++ 0x01-csharp-ifelse_loops_methods/6-print_comb2/6-print_comb2.cs
@@ -6,7 +6,7 @@ namespace _6_print_comb2
     {
         static void Main(string[] args)
         {
-            for (int i = 0; i < 9; i++)
+            for (int i = 0; i < 8; i++)
             {
                 for (int j = i + 1; j < 10; j++)
                 {
@@ -15,7 +15,8 @@ namespace _6_print_comb2
                 }
 
             }
-            // Console.Write("99");
+            Console.WriteLine("89");
         }
     }
 }
+
