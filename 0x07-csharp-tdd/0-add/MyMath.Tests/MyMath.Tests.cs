b92e06bb6a7a66ac6e407351d91ed974ece7370f
Michel Sakr <michel.s.sakr@gmail.com>
2021-07-29 23:19:11 +0300

    first commit

---
+++ 0x07-csharp-tdd/0-add/MyMath.Tests/MyMath.Tests.cs
@@ -0,0 +1,48 @@
+using NUnit.Framework;
+
+namespace MyMath.Tests
+{
+    [TestFixture]
+    /// <summary>
+    /// Tests for addition
+    /// </summary>
+    public class Tests
+    {
+        [Test]
+        /// <summary>
+        /// Two zeros
+        /// </summary>
+        public void TwoZeros()
+        {
+            int twozs = Operations.Add(0, 0);
+            Assert.AreRqual(0, twozs);
+        }
+        
+        /// <summary>
+        /// Two positive
+        /// </summary>
+        public void TwoPositives()
+        {
+            int twos = Operations.Add(1, 9);
+            Assert.AreRqual(10, twos);
+        }
+        
+        /// <summary>
+        /// Two negative
+        /// </summary>
+        public void TwoNegatives()
+        {
+            int twos = Operations.Add(-1, -2);
+            Assert.AreRqual(-3, twos);
+        }
+
+        /// <summary>
+        /// One positive one negative
+        /// </summary>
+        public void Alternative()
+        {
+            int twos = Operations.Add(-1, 1);
+            Assert.AreRqual(0, twos);
+        }
+    }
+}
---
+++ 0x07-csharp-tdd/0-add/MyMath/MyMath.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace MyMath
+{
+    /// <summary>
+    /// Class operations
+    /// </summary>
+    public class Operations
+    {
+        /// <summary>
+        /// Method to add two integers
+        /// </summary>
+        /// <param name="a">int a</param>
+        /// <param name="b">int b</param>
+        /// <returns>addition</returns>
+        public static int Add(int a, int b) {
+            return a + b;
+        }
+    }
+}
