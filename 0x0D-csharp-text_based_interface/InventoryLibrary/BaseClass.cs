3542376ddb4c36dc66554fc93758ab70ccb676e4
Michel Sakr <michel.s.sakr@gmail.com>
2021-11-15 10:18:28 +0200

    0. Base knowledge mandatory

---
+++ 0x0D-csharp-text_based_interface/InventoryLibrary/BaseClass.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace InventoryLibrary
+{
+    public class BaseClass
+    {
+        private string id;
+        private DateTime date_created;
+        private DateTime date_updated;
+    }
+}
